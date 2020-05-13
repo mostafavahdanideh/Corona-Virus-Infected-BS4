c756f3f205a6033526d0ef0f93fc22e5c8695109
Mostafa-mvd <vahdanim77@gmail.com>
2020-05-13 22:46:28 +0430

    Updated

--- corona_virus.py
+++ corona_virus.py
@@ -7,7 +7,7 @@ import csv
 
 #if you want the data to be save in csv file, first write the path of file in the path variable
 #FOR EXAMPLE:
-#path = "/home/believe/py/" or "D:\believe\py\"
+#file_path = "/home/believe/py/" or "D:\believe\py\"
 
 file_path = ""
 
