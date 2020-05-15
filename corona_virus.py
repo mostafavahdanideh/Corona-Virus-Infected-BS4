d5b2f5c7ebbdc039517ea401e7f52c56b64e3e06
Mostafa-mvd <vahdanim77@gmail.com>
2020-05-15 12:53:33 +0430

    current issues resolved

--- corona_virus.py
+++ corona_virus.py
@@ -1,78 +1,105 @@
 #!/usr/bin/python3.8
 
-
 import requests
-from bs4 import BeautifulSoup
 import csv
-
-#if you want the data to be save in csv file, first write the path of file in the path variable
-#FOR EXAMPLE:
-#file_path = "/home/believe/py/" or "D:\believe\py\"
-
-file_path = ""
-
-columns = ["Countries", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered",
-           "Active Cases", "Serious Critical", "Tot Cases", "Deaths", "Total Tests", "Tests"]
+from bs4 import BeautifulSoup
 
 
-try:
+def get_countries_info():
     req = requests.get("https://www.worldometers.info/coronavirus/")
 
     if req.status_code == 200:
         countries_info = []
+
         bs = BeautifulSoup(req.text, "html.parser")
         table = bs.find_all(
             "table", attrs={"id": "main_table_countries_today"})
         tbody = table[0].find_all("tbody")
         trs = tbody[0].find_all("tr", attrs={"style": ""})
 
-        for tr in trs:
+        for tr in trs[1:]:  # start with 1 item because firts item is for world info
             country = []
             tds = tr.find_all("td")
 
-            for td in tds:
+            for td in tds[1:]:  # start with1 item because first item is for show rows
                 text = td.text
                 info = text.split("\n")[0]
+
                 country.append(info)
 
             # this item will be remove becuase this is ALL tab in site's table and we don't need it
             country.pop(-1)
             countries_info.append(country)
 
-        for country_info in countries_info:
-            for idx, value in enumerate(country_info[1:]):
-                real_idx = idx + 1
+        return countries_info
 
-                if value == "":
-                    country_info[real_idx] = "unknown"
-
-                elif (value != "") and (value[0] != "+"):
-                    if "," in value:
-                        number = int(value.replace(",", ""))
-                        country_info[real_idx] = number
-                    elif "." in value:
-                        number = int(value.replace(".", ""))
-                        country_info[real_idx] = number
-
-        # selection sort algorithm for countries
-        for i in range(len(countries_info)):
-            minimum = i
-
-            for j in range(i + 1, len(countries_info)):
-                if int(countries_info[j][1]) > int(countries_info[minimum][1]):
-                    minimum = j
-
-            countries_info[minimum], countries_info[i] = countries_info[i], countries_info[minimum]
-
-        # save information in csv file each time run this script
-        with open(f"{file_path}coronavirus.csv", "w") as corona_csv:
-            countries_info.insert(0, columns)
-            csv_writer = csv.writer(corona_csv, delimiter=",")
-            csv_writer.writerows(countries_info)
-
-        #at the end we have dataset of corona in the world :))))))))
     else:
-        print(req.status_code)
+       raise RuntimeError("Your status code is %i" % (req.status_code))
 
+
+def info_correction(countries_info):
+    for country_info in countries_info:  # countries have list of country
+        # country_info is a list and have info abaut county
+        for idx, value in enumerate(country_info):
+            if idx > 0:  # idx 0 is name of country
+                if value == "":
+                    country_info[idx] = "unknown"
+
+                elif value[0] != "+":
+                    try:
+                        number = int(value)
+                        country_info[idx] = number
+                    except:
+                        if "," in value:
+                            if value[-1] == " ":
+                                value = value.replace(" ", "")
+
+                            number = int(value.replace(",", ""))
+                            country_info[idx] = number
+
+                        elif ("." in value) and (idx != 1):
+                            number = int(value.replace(".", ""))
+                            country_info[idx] = number
+    return countries_info
+
+
+def sort_informations(informations):
+    # selection sort algorithm for information
+    for i in range(len(informations)):
+        minimum = i
+
+        for j in range(i + 1, len(informations)):
+            if int(informations[j][1]) > int(informations[minimum][1]):
+                minimum = j
+
+            informations[minimum], informations[i] = informations[i], informations[minimum]
+
+    return informations
+
+
+def write_in_csv(information, coloums, path):
+    # save information in csv file each time run this script
+    with open(f"{path}coronavirus.csv", "w") as corona_csv:
+        information.insert(0, columns)
+
+        csv_writer = csv.writer(corona_csv, delimiter=",")
+        csv_writer.writerows(information)
+
+
+# if you want the data to be save in csv file, first write the path of file in the path variable
+# FOR EXAMPLE:
+# path = "/home/believe/py/" or "D:\believe\py\"
+
+file_path = "/home/mostafa/ProjectPy/Project/corona/"
+
+columns = ["Countries", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered",
+           "Active Cases", "Serious Critical", "Tot Cases", "Deaths", "Total Tests", "Tests", "Population"]
+
+try:
+    corona_info = get_countries_info()
+    correct_info = info_correction(corona_info)
+    sort_info = sort_informations(correct_info)
+
+    write_in_csv(sort_info, columns, file_path)
 except Exception as error:
     print(error)
